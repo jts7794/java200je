c7b0bb6f0500f40395590cc0b9f0cb229be0f618
taesung <taesung@taesungnoimac>
2020-05-15 18:05:46 +0900

    스트링 컨케트네이션 콘솔확인

--- java200je/src/java200je/VariableTest7.java
+++ java200je/src/java200je/VariableTest7.java
@@ -12,12 +12,24 @@ public class VariableTest7 {
 		System.out.println("2-- 합 10+20="+d);
 		String str ="안녕하세요! JAVA를 즐깁시다.";
 		System.out.println("3-- 문자열 출력 :"+str);
-		//String Concatenation 연속
+		//String Concatenation 연속 문자열에 숫자를 더하면 문자가 된다
 		System.out.println("-- String Concatenation Test--");
 		System.out.println(a+b+" 합 ");
 		System.out.println(a+" 합 "+b);
 		System.out.println(" 합 " + a + b);
 		System.out.println(" 합 "+(a+b));
 	}
+	
+	/* 콘솔 확인
+		1-- 합 10+20=30
+		2-- 합 10+20=30
+		3-- 문자열 출력 :안녕하세요! JAVA를 즐깁시다.
+		-- String Concatenation Test--
+		30 합 
+		10 합 20
+ 		합 1020
+ 		합 30
+
+	 * */
 
 }
