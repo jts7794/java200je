4ef09d814ccc6cc6a66bdf14de2acfcf4863356a
taesung <taesung@taesungnoimac>
2020-05-16 20:42:13 +0900

    상수와 static imports

---
+++ java200je/src/java200je/SansuStatic.java
@@ -0,0 +1,9 @@
+package java200je;
+
+public class SansuStatic {
+
+	//모든 상수는 대문자를 사용한다. 모든 클래스에서 사용 가능(public), 변경 불가(static)
+	public static final int MAN = 1;
+	public static final int WOMAN = 1;
+
+}
