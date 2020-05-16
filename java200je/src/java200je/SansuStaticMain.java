6b17a5ee6f234831c40f58be01876e5c693839e3
taesung <taesung@taesungnoimac>
2020-05-16 20:46:07 +0900

    상수에 대한 설명

--- java200je/src/java200je/SansuStaticMain.java
+++ java200je/src/java200je/SansuStaticMain.java
@@ -2,6 +2,13 @@ package java200je;
 
 public class SansuStaticMain {
 	
+	/* 상수를 정의한 클래스와 사용하는 클래스가 다를 경우 '클래스명.상수(ex) SansuStatic.WOMAN의 형식)'
+	 * 여기서 클래스명은 상수를 선언한 클래스이다.
+	 * 
+	 * 클래스 내에서 선언하는 상수는 클래스명을 붙이지 않고 WOMEN, MEN과 같은 형식으로 사용한다.
+	 * 선언된 상수는 API에 있으며 주로 사용하는 상수는 java.lang.Math클래스에 있다.
+	 * */
+	
 	public final static int MEN = 3;
 	public final static int WOMEN = 4;
 
