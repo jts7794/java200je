77c2eba8506be24960c9446019b9a78b6da2ebaf
taesung <taesung@imac>
2020-07-24 14:52:29 +0900

    51.문자 피라미드 출력하기 (char)('a'+1)=b

---
+++ java200je/src/java200je/NestedFor.java
@@ -0,0 +1,85 @@
+package java200je;
+
+public class NestedFor {
+
+	public static void main(String[] args) {
+
+		int count = 5;
+		printButterfly(count);
+		printDiamond(count);
+	}//
+	
+	public static void printChar(int n) {
+		int inters = n;
+		for(int i=0; i<inters; i++) {
+			for(int j=0; j<inters-1-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int m = 0; m <=i ; m++) {
+				System.out.print((char)('a'+m));
+			}
+			
+			for(int k=inters-1-i; k <=inters-1 ; k++) {
+				System.out.print((char)('a'+inters-1-k));
+			}
+			System.out.println();
+		}
+	}//
+	
+	public static void printButterflyChar(int n) {
+		int inters = n;
+		for(int i=0; i <inters; i++) {
+			for(int j=0; j <i ; j++) {
+				System.out.print(" ");
+			}
+			for(int m =0 ; m<=inters-1-i ; m++) {
+				System.out.print((char)('a'+m));
+			}
+			
+			for(int k = inters-1-i; k>=0; k--) {
+				System.out.print((char)('a'+k));
+			}
+			System.out.println();
+		}
+	}//
+
+	public static void printButterfly(int n) {
+		printChar(n);
+		printButterflyChar(n);
+		System.out.println("-----------------------");
+	}//
+	
+	public static void printDiamond(int n) {
+		printButterflyChar(n);
+		printChar(n);
+		System.out.println("-----------------------");
+	}
+}
+/*console 
+    aa
+   abba
+  abccba
+ abcddcba
+abcdeedcba
+abcdeedcba
+ abcddcba
+  abccba
+   abba
+    aa
+-----------------------
+abcdeedcba
+ abcddcba
+  abccba
+   abba
+    aa
+    aa
+   abba
+  abccba
+ abcddcba
+abcdeedcba
+-----------------------
+
+*/
+
+
